fe99d21887638f9c08fed5918c7c756784ce02ad
Jack Lukic <jack@semantic-ui.com>
2015-02-18 14:32:51 -0500

    Updated component to version 1.9.1

---
+++ package.js
@@ -0,0 +1,17 @@
+var
+  where = 'client' // Adds files only to the client
+;
+
+Package.describe({
+  name    : 'semantic:ui-icon',
+  summary : 'Semantic UI - Icon (official): Single component release of icon',
+  version : '1.9.1',
+  git     : 'git://github.com/Semantic-Org/UI-Icon.git',
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles([
+    
+  ], where);
+});
