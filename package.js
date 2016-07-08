2641e52a8de0678b556cc069b921fa86c041e834
Pusher Robot <jack@semantic-ui.com>
2016-07-08 17:02:43 -0400

    Updated component to version 2.2.2

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-icon',
   summary : 'Semantic UI - Icon: Single component release',
-  version : '2.2.1',
+  version : '2.2.2',
   git     : 'git://github.com/Semantic-Org/UI-Icon.git',
 });
 
