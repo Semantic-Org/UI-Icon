698260eae18be5c995470b2dc4ef9bbdae91ae01
Jack Lukic <jack@semantic-ui.com>
2017-03-29 06:41:33 -0400

    Updated component to version 2.2.10

--- package.js
+++ package.js
@@ -2,7 +2,7 @@
 Package.describe({
   name    : 'semantic:ui-icon',
   summary : 'Semantic UI - Icon: Single component release',
-  version : '2.2.8',
+  version : '2.2.10',
   git     : 'git://github.com/Semantic-Org/UI-Icon.git',
 });
 
